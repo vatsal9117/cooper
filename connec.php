6187dfb43f64333fd74157808e589117be8e94ec
Vatsal Dalal <67739998+vatsal9117@users.noreply.github.com>
2020-12-01 11:30:43 +0530

    Add files via upload

---
+++ connec.php
@@ -0,0 +1,24 @@
+<?php
+
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $PhoneNumber = $_POST['PhoneNumber'];
+
+    //database connection:
+    $conn = new mysqli('localhost','root','','test');
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error );
+    }else
+    {
+        $stmt = $conn->prepare("insert into registration(fname,lname,email,password,PhoneNumber)values(?,?,?,?,?)");
+        $stmt->bind_param("ssssi",$fname, $lname, $email, $password,$PhoneNumber);
+        $stmt->execute();
+        echo"Registration Complete.";
+        $stmt->close();
+        $conn->close();
+    }
+
+
+?>
